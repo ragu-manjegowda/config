9ab89ec7d05887ea2d9c83162d6836b3941b8d24
Ragu Manjegowda <raghavendrahm0410@gmail.com>
2021-12-13 06:24:01 -0800

    [feat]: FZF, Add fzf support for bash
    Details:
        Adding fzf support in bash
    
    Resolves:
     * FZF in bash
    
    Tests:
     * Tested on Ubuntu 20.04
    
    Signed-off-by: Ragu Manjegowda <raghavendrahm0410@gmail.com>

---
+++ .config/bash/.fzf.bash
@@ -0,0 +1,14 @@
+# Setup fzf
+# ---------
+if [[ ! "$PATH" == */home/linuxbrew/.linuxbrew/Cellar/fzf/0.28.0/bin* ]]; then
+    export PATH="${PATH:+${PATH}:}$(brew --prefix)/Cellar/fzf/0.28.0/bin"
+fi
+
+# Auto-completion
+# ---------------
+[[ $- == *i* ]] && source "$(brew --prefix)/Cellar/fzf/0.28.0/shell/completion.bash" 2> /dev/null
+
+# Key bindings
+# ------------
+source "$(brew --prefix)/Cellar/fzf/0.28.0/shell/key-bindings.bash"
+
