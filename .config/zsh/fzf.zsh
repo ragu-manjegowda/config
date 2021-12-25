134c2a27d0f21cbc5d8ea26529a35f457d1f1fa5
Ragu Manjegowda <raghavendrahm0410@gmail.com>
2021-12-25 14:50:02 -0800

    [update]: fzf, Update path to shell utils.
    Details:
        Update path such that rc need not be re-configured after upgrade.
    
    Resolves:
     * rc updates after fzf upgrade.
    
    Tests:
     * Tested on Ubuntu 20.04.
    
    Signed-off-by: Ragu Manjegowda <raghavendrahm0410@gmail.com>

--- .config/zsh/fzf.zsh
+++ .config/zsh/fzf.zsh
@@ -1,13 +1,10 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */home/linuxbrew/.linuxbrew/Cellar/fzf/0.28.0/bin* ]]; then
-    export PATH="${PATH:+${PATH}:}$(brew --prefix)/Cellar/fzf/0.28.0/bin"
-fi
-
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "$(brew --prefix)/Cellar/fzf/0.28.0/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "$(brew --prefix)/opt/fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "$(brew --prefix)/Cellar/fzf/0.28.0/shell/key-bindings.zsh"
+source "$(brew --prefix)/opt/fzf/shell/key-bindings.zsh"
+
