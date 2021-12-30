838ae43cccf7bc1cbee464a308de3beae564c50e
Ragu Manjegowda <raghavendrahm0410@gmail.com>
2021-12-30 02:27:19 -0800

    [feat]: awesomewm, Add useless gaps to topbar.
    Details:
        Tweaked top panel to add margins and background only to widgets.
    Also removed bluetooth and updater from top panel.
    
    Resolves:
     * Remove background from top panel wibox.
    
    Tests:
     * Tested on Ubuntu 20.04.
    
    Signed-off-by: Ragu Manjegowda <raghavendrahm0410@gmail.com>

--- .config/awesome/layout/top-panel.lua
+++ .config/awesome/layout/top-panel.lua
@@ -15,19 +15,19 @@ local top_panel = function(s)
 	{
 		ontop = true,
 		screen = s,
-		type = 'dock',
-		height = dpi(28),
+		type = 'desktop',
+		height = dpi(33),
 		width = s.geometry.width,
 		x = s.geometry.x,
 		y = s.geometry.y,
 		stretch = false,
-		bg = beautiful.background,
+		bg = "#0000", -- fake transparency (uses wallpaper as background)
 		fg = beautiful.fg_normal
 	}
 
 	panel:struts
 	{
-		top = dpi(28)
+		top = dpi(33)
 	}
 
 	panel:connect_signal(
@@ -51,9 +51,7 @@ local top_panel = function(s)
 	local clock 			= require('widget.clock')(s)
 	local layout_box 		= require('widget.layoutbox')(s)
 	s.tray_toggler  		= require('widget.tray-toggle')
-	s.updater 				= require('widget.package-updater')()
 	s.screen_rec 			= require('widget.screen-recorder')()
-	s.bluetooth   			= require('widget.bluetooth')()
 	s.battery     			= require('widget.battery')()
 	s.vpn     			    = require('widget.vpn')()
 	s.network       		= require('widget.network')()
@@ -62,36 +60,64 @@ local top_panel = function(s)
 	s.info_center_toggle 	= require('widget.info-center-toggle')()
 
 	panel : setup {
-		layout = wibox.layout.align.horizontal,
-		expand = 'none',
-		{
-			layout = wibox.layout.fixed.horizontal,
-            vseparator,
-            tag_list.create(s),
-            vseparator,
-			task_list(s),
-		},
-		clock,
-		{
-			layout = wibox.layout.fixed.horizontal,
-			spacing = dpi(5),
-			{
-				s.systray,
-				margins = dpi(5),
-				widget = wibox.container.margin
-			},
-			s.tray_toggler,
-			s.updater,
-			s.screen_rec,
-			s.network,
-			s.bluetooth,
-			s.battery,
-            s.vpn,
-			s.control_center_toggle,
-			s.global_search,
-			layout_box,
-			s.info_center_toggle
-		}
+        {
+	    	layout = wibox.layout.align.horizontal,
+	    	expand = 'none',
+	    	{
+                {
+	    	        type = 'dock',
+	    		    layout = wibox.layout.fixed.horizontal,
+                    vseparator,
+                    tag_list.create(s),
+                    vseparator,
+	    		    task_list(s),
+                },
+
+                bg = beautiful.background,
+                shape = gears.shape.rounded_rect,
+                widget = wibox.container.background,
+	    	},
+            {
+                {
+	    	        type = 'dock',
+                    layout = wibox.layout.fixed.horizontal,
+                    clock,
+                },
+
+                bg = beautiful.background,
+                shape = gears.shape.rounded_rect,
+                widget = wibox.container.background,
+            },
+	    	{
+                {
+	    	        type = 'dock',
+	    		    layout = wibox.layout.fixed.horizontal,
+	    		    spacing = dpi(5),
+	    		    {
+	    		    	s.systray,
+	    		    	margins = dpi(5),
+	    		    	widget = wibox.container.margin
+	    		    },
+	    		    s.tray_toggler,
+	    		    s.screen_rec,
+	    		    s.network,
+	    		    s.battery,
+                    s.vpn,
+	    		    s.control_center_toggle,
+	    		    s.global_search,
+	    		    layout_box,
+	    		    s.info_center_toggle
+                },
+
+                bg = beautiful.background,
+                shape = gears.shape.rounded_rect,
+                widget = wibox.container.background,
+	    	},
+        },
+        left = dpi(25),
+        right = dpi(5),
+        top = dpi(5),
+        widget = wibox.container.margin,
 	}
 
 	return panel
