33a09d5e06cc7a9cbed600ac77d559a33b40f746
Ragu Manjegowda <raghavendrahm0410@gmail.com>
2022-01-26 00:18:25 -0800

    [feat/update]: archiso, Backup packages
    Details:
        Added hooks to run when installing.removing/upgrading packages.
    Fixed issues in running backing up pakcages hook.
    
    Resolves:
     * Pacman and AUR packages backup.
    
    Tests:
     * Tested on Arch Linux.
    
    Signed-off-by: Ragu Manjegowda <raghavendrahm0410@gmail.com>

--- .config/archiso-backup/backup-archiso.sh
+++ .config/archiso-backup/backup-archiso.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Backup all packages
-pacman -Qqe > pkglistAll.txt
+pacman -Qqe > /var/archiso-backup/pkglistAll.txt
 
 # Backup packman installed packages
-pacman -Qqen > pkglistPacmanInstalled.txt
+pacman -Qqen > /var/archiso-backup/pkglistPacmanInstalled.txt
 
 # Backup AUR/foreign packages
-pacman -Qqem > foreignpkglist.txt
+pacman -Qqem > /var/archiso-backup/foreignpkglist.txt
