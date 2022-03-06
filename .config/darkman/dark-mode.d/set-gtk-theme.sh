d7510ff5e4199f49facc6fc9bc3403c9a0cafdbb
Ragu Manjegowda <raghavendrahm0410@gmail.com>
2022-03-06 21:14:42 +0530

    [feat]: gtk, enable solarized theme
    Details:
        Add support for solarized theme for gtk apps and make it default.
    
    Resolves:
     * Solarized for gtk.
    
    Tests:
     * Tested on Arch Linux.
    
    Signed-off-by: Ragu Manjegowda <raghavendrahm0410@gmail.com>

--- .config/darkman/dark-mode.d/set-gtk-theme.sh
+++ .config/darkman/dark-mode.d/set-gtk-theme.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 
 xdg_config_path="$HOME/.config/xsettingsd/xsettingsd.conf"
-sed -i -e "s#-light#-dark#g" $xdg_config_path
+sed -i -e "s#LightBlue#DarkGreen#g" $xdg_config_path
 
 if ! pgrep -x "xsettingsd" > /dev/null
 then
