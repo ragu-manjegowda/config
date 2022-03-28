b360d4d74617151923dba587bf6949a9678c27ce
Ragu Manjegowda <raghavendrahm0410@gmail.com>
2022-03-28 10:12:18 +0530

    [fix]: awesome, fix brightness osd color
    Details:
        Fixed brightness osd color to adapt with theme change.
    
    Resolves:
     * Broken color changes with theme.
    
    Tests:
     * Tested on Arch Linux.
    
    Signed-off-by: Ragu Manjegowda <raghavendrahm0410@gmail.com>

--- .config/awesome/widget/volume-slider/init.lua
+++ .config/awesome/widget/volume-slider/init.lua
@@ -87,36 +87,6 @@ volume_slider:connect_signal(
 	end
 )
 
-volume_slider:buttons(
-	gears.table.join(
-		awful.button(
-			{},
-			4,
-			nil,
-			function()
-				if volume_slider:get_value() > 100 then
-					volume_slider:set_value(100)
-					return
-				end
-				volume_slider:set_value(volume_slider:get_value() + 5)
-			end
-		),
-		awful.button(
-			{},
-			5,
-			nil,
-			function()
-				if volume_slider:get_value() < 0 then
-					volume_slider:set_value(0)
-					return
-				end
-				volume_slider:set_value(volume_slider:get_value() - 5)
-			end
-		)
-	)
-)
-
-
 local update_slider = function()
 	awful.spawn.easy_async_with_shell(
 		[[bash -c "amixer -D pulse sget Master"]],
