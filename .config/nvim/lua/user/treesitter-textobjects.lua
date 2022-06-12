15fe94ae7ee524fc865b2ac7f9d0b7c385b92707
Ragu Manjegowda <raghavendrahm0410@gmail.com>
2022-06-12 03:37:02 -0700

    [add/feat]: nvim, treesitter-textobject
    Details:
        Add treesitter-textobject plugin for better method navigation instead of
    using regex based native mappings.
    
    Resolves:
     * Navigate between methods and class.
    
    Tests:
     * Tested on Arch Linux.
    
    Signed-off-by: Ragu Manjegowda <raghavendrahm0410@gmail.com>

---
+++ .config/nvim/lua/user/treesitter-textobjects.lua
@@ -0,0 +1,54 @@
+local M = {}
+
+M.config = function()
+    require'nvim-treesitter.configs'.setup {
+        textobjects = {
+            select = {
+                enable = true,
+
+                -- Automatically jump forward to textobj, similar to targets.vim
+                lookahead = true,
+
+                keymaps = {
+                    -- You can use the capture groups defined in textobjects.scm
+                    ["af"] = "@function.outer",
+                    ["if"] = "@function.inner",
+                    ["ac"] = "@class.outer",
+                    ["ic"] = "@class.inner",
+                },
+            },
+
+            move = {
+                enable = true,
+                set_jumps = true, -- whether to set jumps in the jumplist
+                goto_next_start = {
+                    ["]m"] = "@function.outer",
+                    ["]c"] = "@class.outer",
+                },
+                goto_next_end = {
+                    ["]M"] = "@function.outer",
+                    ["]C"] = "@class.outer",
+                },
+                goto_previous_start = {
+                    ["[m"] = "@function.outer",
+                    ["[c"] = "@class.outer",
+                },
+                goto_previous_end = {
+                    ["[M"] = "@function.outer",
+                    ["[C"] = "@class.outer",
+                },
+            },
+
+            lsp_interop = {
+                enable = true,
+                border = 'none',
+                peek_definition_code = {
+                    ["<leader>df"] = "@function.outer",
+                    ["<leader>dc"] = "@class.outer",
+                },
+            },
+        },
+    }
+end
+
+return M
