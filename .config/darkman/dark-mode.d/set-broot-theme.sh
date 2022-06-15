91052620a7dbf2e46a325b7ef21799a214a798bc
Ragu Manjegowda <raghavendrahm0410@gmail.com>
2022-06-15 15:24:30 -0700

    [fix]: broot, fix skin
    Details:
        Fix skin theme so that it won't move the template but copy.
    
    Resolves:
     * Broot dark and light skin.
    
    Tests:
     * Tested on Arch Linux.
    
    Signed-off-by: Ragu Manjegowda <raghavendrahm0410@gmail.com>

--- .config/darkman/dark-mode.d/set-broot-theme.sh
+++ .config/darkman/dark-mode.d/set-broot-theme.sh
@@ -3,4 +3,4 @@
 broot_config_path="$HOME/.config/broot/conf.hjson"
 broot_dark_config_path="$HOME/.config/broot/conf-dark.hjson"
 
-mv $broot_dark_config_path $broot_config_path
+cp $broot_dark_config_path $broot_config_path
--- .config/darkman/light-mode.d/set-broot-theme.sh
+++ .config/darkman/light-mode.d/set-broot-theme.sh
@@ -3,4 +3,4 @@
 broot_config_path="$HOME/.config/broot/conf.hjson"
 broot_light_config_path="$HOME/.config/broot/conf-light.hjson"
 
-mv $broot_light_config_path $broot_config_path
+cp $broot_light_config_path $broot_config_path
