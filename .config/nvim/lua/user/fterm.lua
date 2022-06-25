ba6db24210cbb58b8dd9e3936252b8035851a223
Ragu Manjegowda <raghavendrahm0410@gmail.com>
2022-06-24 23:28:42 -0700

    [add/fix]: nvim, support to send commands to shell
    Details:
     * Added support to send command to shell.
     * Got rid of global functions in configs.
     * Updated keymaps that open things to start with 'o'.
    
    Resolves:
     * Send commands to shell.
    
    Tests:
     * Tested on Arch Linux.
    
    Signed-off-by: Ragu Manjegowda <raghavendrahm0410@gmail.com>

--- .config/nvim/lua/user/fterm.lua
+++ .config/nvim/lua/user/fterm.lua
@@ -2,6 +2,50 @@ local vim = vim
 
 local M = {}
 
+local ftzsh = require("FTerm"):new({
+    ft = 'fterm_zsh',
+    cmd = "zsh",
+    dimensions = {
+        height = 0.9,
+        width = 0.9
+    }
+})
+
+-- Use this to toggle zsh in a floating terminal
+function M.__fterm_zsh()
+    ftzsh:toggle()
+end
+
+function M.terminal_send(text)
+    first_terminal_chan = get_first_terminal()
+
+    if first_terminal_chan == "" or first_terminal_chan == nil then
+        vim.notify("[fterm] " .. "No open terminal available", vim.log.levels.WARN)
+    else
+        vim.api.nvim_chan_send(first_terminal_chan, text)
+    end
+end
+
+function get_first_terminal()
+    terminal_chans = {}
+
+    for _, chan in pairs(vim.api.nvim_list_chans()) do
+        if chan["mode"] == "terminal" and chan["pty"] ~= "" then
+            table.insert(terminal_chans, chan)
+        end
+    end
+
+    table.sort(terminal_chans, function(left, right)
+        return left["buffer"] < right["buffer"]
+    end)
+
+    if next(terminal_chans) == nil then
+        return nil
+    else
+        return terminal_chans[1]["id"]
+    end
+end
+
 function M.config()
     require'FTerm'.setup({
         border = 'double',
@@ -11,26 +55,13 @@ function M.config()
         },
     })
 
-    local ftzsh = require("FTerm"):new({
-        ft = 'fterm_zsh', -- You can also override the default filetype, if you want
-        cmd = "zsh",
-        dimensions = {
-            height = 0.9,
-            width = 0.9
-        }
-    })
-
-     -- Use this to toggle zsh in a floating terminal
-    function _G.__fterm_zsh()
-        ftzsh:toggle()
-    end
-
-    -- Example keybindings
+    -- Keybindings
     local map = vim.api.nvim_set_keymap
     local opts = { noremap = true, silent = true }
 
-    map('n', '<Leader>ts', '<CMD>lua __fterm_zsh()<CR>', opts)
-    map('t', '<Leader>ts', '<C-\\><C-n><CMD>lua __fterm_zsh()<CR>', opts)
+    map('n', '<leader>ts', '<CMD>lua require("user.fterm").__fterm_zsh()<CR>', opts)
+    map('t', '<leader>ts', '<C-\\><C-n><CMD>lua require("user.fterm").__fterm_zsh()<CR>', opts)
+    map('n', '<leader>ss', '<CMD>lua require("user.fterm").terminal_send(vim.fn.input("> ") .. "\\n")<CR>', opts)
 end
 
 return M
