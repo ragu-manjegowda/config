6882bb2d21136e3545ebe7004d08a9caa1a1bf11
Ragu Manjegowda <raghavendrahm0410@gmail.com>
2022-06-28 00:27:04 -0700

    [chor]: config, update submodules
    Details:
        Update submodules and have them track master ToT.
    
    Resolves:
     * Keep submodules updated.
    
    Tests:
     * Tested on Arch Linux.
    
    Signed-off-by: Ragu Manjegowda <raghavendrahm0410@gmail.com>

--- .config/zsh/zsh-custom/plugins/custom-alias/custom-alias.plugin.zsh
+++ .config/zsh/zsh-custom/plugins/custom-alias/custom-alias.plugin.zsh
@@ -73,7 +73,7 @@ alias gpulla='git pull --rebase --autostash'
 
 alias gst='git status'
 alias gstv='vim +Git +only'
-alias gsu='git submodule update --init --recursive'
+alias gsu='git submodule update --remote --rebase'
 
 function grename() {
   if [[ -z "$1" || -z "$2" ]]; then
