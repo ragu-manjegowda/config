c42e968fecae63f6c33b8bda04edd629fd1cdf31
Ragu Manjegowda <raghavendrahm0410@gmail.com>
2022-09-05 00:41:01 -0700

    [fix]: zathura, color theme
    Details:
        Fixed broken solarized color theme, also added darkman scripts to
    update theme based on mode.
    
    Resolves:
     * Dark/Light solarized theme for zathura.
    
    Tests:
     * Tested on Arch with zathura 0.4.9
    
    Signed-off-by: Ragu Manjegowda <raghavendrahm0410@gmail.com>

---
+++ .config/darkman/dark-mode.d/set-zathura-theme.sh
@@ -0,0 +1,7 @@
+#!/bin/zsh
+
+zathura_config_path="$HOME/.config/zathura/zathurarc"
+zathura_dark_config_path="$HOME/.config/zathura/zathurarc-dark"
+
+cp $zathura_dark_config_path $zathura_config_path
+
---
+++ .config/darkman/light-mode.d/set-zathura-theme.sh
@@ -0,0 +1,7 @@
+#!/bin/zsh
+
+zathura_config_path="$HOME/.config/zathura/zathurarc"
+zathura_light_config_path="$HOME/.config/zathura/zathurarc-light"
+
+cp $zathura_light_config_path $zathura_config_path
+
