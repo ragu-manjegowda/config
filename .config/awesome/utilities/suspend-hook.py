e392015e458ab3e84e75b9c55f195397a3559c3b
Ragu Manjegowda <raghavendrahm0410@gmail.com>
2022-09-06 03:02:20 -0700

    [cleanup]: awesomewm, start-up apps
    Details:
        Cleanup start up apps commands, modified suspend-hook script to not
    spawn new process on every reload.
    
    Resolves:
     * Do not spawn new process everytime awesome is reloaded.
    
    Tests:
     * Tested on Arch Linux with Awesome v4.3-1521-gc8d4f878a
    
    Signed-off-by: Ragu Manjegowda <raghavendrahm0410@gmail.com>

--- .config/awesome/utilities/suspend-hook.py
+++ .config/awesome/utilities/suspend-hook.py
@@ -1,8 +1,27 @@
 #!/usr/bin/env python3
 
+###############################################################################
+# This script uses dbus to detect when the system returns from hibernation
+# or suspend.
+#
+# Reference - https://github.com/CatoMaior/Dotfiles/blob/main/Scripts/moveVitals
+# Modified by - Ragu Manjegowda
+# Github - @ragu-manjegowda
+###############################################################################
+
+
 from os import system
-from gi.repository import GLib
-from gi.repository import Gio
+from gi.repository import GLib, Gio
+from subprocess import run
+
+# Check if this is already running, if so exit
+p = run("pgrep -f suspend-hook.py | wc -l", capture_output=True, shell=True)
+
+# Check for length 2 as current instance of process creates a PID as well
+# and wc -l above counts extra '\n' from pgrep output.
+if p.stdout.strip().decode('utf-8') != '2':
+    # print(p.stdout.strip().decode('utf-8'))
+    exit()
 
 def onPrepareForSleep(conn, sender, obj, interface, signal, parameters, data):
     if not parameters[0]: # parameters[0] is True just before sleep, False just after wake
