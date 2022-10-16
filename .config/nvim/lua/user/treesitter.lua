a095b8c9c66bc2bb3ad0834f5ca77f87446dec3c
Ragu Manjegowda <raghavendrahm0410@gmail.com>
2022-10-16 13:40:28 -0700

    [chor]: config, update submodules, fix misc stuff
    Details:
     * Fixed alacritty deprecated warning.
     * Cleanup treesitter redundant config.
     * Fix awesome keymaps.
     * Update submodules.
    
    Resolves:
     * None
    
    Tests:
     * Tested on Arch Linux.
    
    Signed-off-by: Ragu Manjegowda <raghavendrahm0410@gmail.com>

--- .config/nvim/lua/user/treesitter.lua
+++ .config/nvim/lua/user/treesitter.lua
@@ -2,10 +2,6 @@ local M = {}
 
 M.config = function()
     require'nvim-treesitter.configs'.setup {
-        highlight = {
-            enable = true,
-            disable = {},
-        },
         indent = {
             enable = false,
             disable = {},
