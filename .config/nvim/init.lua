d05234b51fad404b6c14871f6d39faf7cb60d2a3
Ragu Manjegowda <raghavendrahm0410@gmail.com>
2022-12-27 13:22:59 -0800

    [add]: nvim, format using LSP
    Details:
        Add key map to format code using LSP.
    
    Resolves:
     * Formatting with LSP.
    
    Tests:
     * Tested on Mac Monterey with nvim 0.8.1
    
    Signed-off-by: Ragu Manjegowda <raghavendrahm0410@gmail.com>

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,14 +1,30 @@
 -- References
 -- https://github.com/zhuorantan/dotfiles
 -- https://github.com/LunarVim/LunarVim
+
+-- Load all static (non plugin based) settings
 require "user.options"
 require "user.keymaps"
 require "user.autocommands"
 require "user.colorscheme"
 require "user.buf-only"
 
-require("user.bootstrap").init()
+-- Bootstrap packer
+local res, bootstrap = pcall(require, "user.bootstrap")
+if not res then
+    vim.notify("Bootstrapping not available", vim.log.levels.ERROR)
+    return
+end
 
-local plugins = require "user.plugins-table"
-require("user.bootstrap").load { plugins }
+bootstrap.init()
 
+-- Load plugins
+local plugins
+
+res, plugins = pcall(require, "user.plugins-table")
+if not res then
+    vim.notify("Plugins table not found", vim.log.levels.ERROR)
+    return
+end
+
+bootstrap.load { plugins }
