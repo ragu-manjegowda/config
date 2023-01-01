0174e39b0b95664359a5c893715b296a7ecd4fc3
Ragu Manjegowda <raghavendrahm0410@gmail.com>
2022-12-31 17:17:50 -0800

    [add]: nvim, keymap to trigger omni completion
    Details:
        Adding keymap <c-d> to trigger omni completion from nvim native lsp
    when nvim-cmp has no completion.
    
    Resolves:
     * Completion for lua code.
    
    Tests:
     * Tested on Arch with nvim 0.8.2-1
    
    Signed-off-by: Ragu Manjegowda <raghavendrahm0410@gmail.com>

--- .config/nvim/lua/user/nvim-cmp.lua
+++ .config/nvim/lua/user/nvim-cmp.lua
@@ -63,12 +63,12 @@ function M.config()
             ['<Down>'] = cmp.mapping(
                 cmp.mapping.select_next_item(
                     { behavior = cmp.SelectBehavior.Select }),
-                    {'i'}
+                    { 'i' }
             ),
             ['<Up>'] = cmp.mapping(
                 cmp.mapping.select_prev_item(
                     { behavior = cmp.SelectBehavior.Select }),
-                    {'i'}
+                    { 'i' }
             ),
             ['<C-n>'] = cmp.mapping({
                 c = function()
@@ -102,9 +102,18 @@ function M.config()
                     end
                 end
             }),
-            ['<C-u>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), {'i', 'c'}),
-            ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), {'i', 'c'}),
-            ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), {'i', 'c'}),
+            ['<C-u>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
+            ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
+            ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
+
+            ['<C-d>'] = cmp.mapping(cmp.mapping.complete({
+                            config = {
+                                sources = {
+                                    { name = 'omni' },
+                                }
+                            }
+                        }), { 'i' }),
+
             ['<C-e>'] = cmp.mapping({
               i = cmp.mapping.close(), c = cmp.mapping.close() }),
             ['<CR>'] = cmp.mapping({
@@ -159,7 +168,7 @@ function M.config()
 
                 return vim_item
             end,
-            fields = {'menu', 'abbr', 'kind'}
+            fields = { 'menu', 'abbr', 'kind' }
         },
 
         window = {
