705677215fc92ed5f311919cf5bdea03c20e1b0d
Ragu Manjegowda <raghavendrahm0410@gmail.com>
2023-01-17 00:58:12 -0800

    [fix]: nvim, fix italics
    Details:
        Fix broken italics in color scheme.
    
    Resolves:
     * Broken italic in highlight group.
    
    Tests:
     * Tested on Arch with nvim 0.8.2
    
    Signed-off-by: Ragu Manjegowda <raghavendrahm0410@gmail.com>

--- .config/nvim/lua/user/colorscheme.lua
+++ .config/nvim/lua/user/colorscheme.lua
@@ -1,6 +1,6 @@
 local ok_status, NeoSolarized = pcall(require, "NeoSolarized")
 
--- If plugin is available leverage it
+-- If plugin is available leverage it (needed for lspsaga)
 if ok_status then
     local theme = "light"
 
@@ -10,7 +10,7 @@ if ok_status then
         transparent = false
     }
 else
-    -- Notify user about missing plugin, go on to use fallback static file from
+    -- Notify user about missing plugin, go on to use static file from
     -- ~/.config/nvim/colors/NeoSolarized.vim
     vim.notify("NeoSolarized not found", vim.log.levels.WARN)
 end
@@ -29,11 +29,19 @@ vim.cmd [[
         let g:neosolarized_contrast = "high"
         let g:neosolarized_visibility = "high"
         let g:neosolarized_termcolors=256
-        let g:neosolarized_bold = 1
-        let g:neosolarized_underline = 1
-        let g:neosolarized_italic = 0
         colorscheme NeoSolarized
     catch /^Vim\%((\a\+)\)\=:E18o
         colorscheme default
     endtry
 ]]
+
+-- For whatever reason italic does not work, hence do the following to
+-- force italic for those defined in colorscheme
+local function update_hl(group, tbl)
+    local old_hl = vim.api.nvim_get_hl_by_name(group, true)
+    local new_hl = vim.tbl_extend('force', old_hl, tbl)
+    vim.api.nvim_set_hl(0, group, new_hl)
+end
+
+update_hl('Comment', { italic = true })
+update_hl('gitcommitComment', { italic = true })
