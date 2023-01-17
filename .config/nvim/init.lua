f1e29d3a755f4d670d95e0f769045975909ede88
Ragu Manjegowda <raghavendrahm0410@gmail.com>
2023-01-17 01:59:38 -0800

    [fix]: nvim, disable hlsearch
    Details:
        Disable hlsearch since autocmd takes care of highlighting the
    searched word only while searching and clear it later.
    
    Resolves:
     * No highlight for `*`, `#`, `N` and `n`.
    
    Tests:
     * Tested with nvim 0.8.2
    
    Signed-off-by: Ragu Manjegowda <raghavendrahm0410@gmail.com>

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -9,6 +9,10 @@ require "user.autocommands"
 require "user.colorscheme"
 require "user.buf-only"
 
+-- other way is to put the above in custom directory and make package of
+-- its own vim.opt.rtp:append(vim.fn.stdpath "config" .. "/custom")
+-- https://github.com/NvChad/NvChad/issues/605#issuecomment-1001109229
+
 -- Bootstrap packer
 local res, bootstrap = pcall(require, "user.bootstrap")
 if not res then
