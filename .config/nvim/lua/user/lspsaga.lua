062a1caa6d58b22b90e6586ac2ceebf220c0f5da
Ragu Manjegowda <raghavendrahm0410@gmail.com>
2023-01-16 17:48:46 -0800

    [update]: nvim, lspsaga breaking changes
    Details:
        Update config after lspsaga's breaking changes.
    
    Resolves:
     * Plugin config maintanance.
    
    Tests:
     * Tested on Arch with nvim 0.8.2
    
    Signed-off-by: Ragu Manjegowda <raghavendrahm0410@gmail.com>

--- .config/nvim/lua/user/lspsaga.lua
+++ .config/nvim/lua/user/lspsaga.lua
@@ -9,70 +9,111 @@ function M.config()
         return
     end
 
-    saga.init_lsp_saga({
+    -- Setup SagaUI colors
+    local colors
+    res, colors = pcall(require, "NeoSolarized.colors")
+    if not res then
+        vim.notify("NeoSolarized.colors not found", vim.log.levels.ERROR)
+        return
+    end
+
+    colors = colors.setup()
+
+    saga.setup({
         border_style = "rounded",
-        finder_action_keys = {
-            open = '<CR>',
-            quit = 'q',
-            tabe = '<leader><Tab>',
+        callhierarchy = {
+            keys = {
+                tabe = '<leader><Tab>',
+                expand_collaspe = '<CR>',
+            },
         },
-        code_action_keys = {
-            exec = '<CR>',
-            quit = 'q',
-        },
-        definition_action_keys = {
+        definition = {
             edit = '<CR>',
             quit = 'q',
             tabe = '<leader><Tab>',
         },
+        diagnostic = {
+            show_code_action = false,
+        },
+        finder = {
+            edit = '<CR>',
+            quit = 'q',
+            tabe = '<leader><Tab>',
+        },
+        outline = {
+            keys = {
+                expand_collaspe = '<CR>',
+            }
+        },
         symbol_in_winbar = {
-            in_custom = true
-        }
+            show_file = false,
+        },
+        ui = {
+            -- border type can be single,double,rounded,solid,shadow.
+            border = 'rounded',
+            winblend = 0,
+            colors = {
+                --float window normal bakcground color
+                normal_bg = colors.bg1,
+                --title background color
+                title_bg = colors.bg4,
+                red = colors.red,
+                magenta = colors.aqua,
+                orange = colors.orange,
+                yellow = colors.yellow,
+                green = colors.green,
+                cyan = colors.violet,
+                blue = colors.blue,
+                purple = colors.purple,
+                white = colors.yellow,
+                black = colors.orange,
+            }
+        },
     })
 
     local map = vim.api.nvim_set_keymap
 
     -- LSP
     map('n', '[d', '<cmd>Lspsaga diagnostic_jump_prev<CR>',
-    { silent = true, desc = 'LSP jump to prev diagnostics' })
+        { silent = true, desc = 'LSP jump to prev diagnostics' })
 
     map('n', ']d', '<cmd>Lspsaga diagnostic_jump_next<CR>',
-    { silent = true, desc = 'LSP jump to next diagnostics' })
+        { silent = true, desc = 'LSP jump to next diagnostics' })
 
     map('n', '[e',
-    "<cmd>lua require('lspsaga.diagnostic').goto_prev({ severity = vim.diagnostic.severity.ERROR })<CR>",
-    { silent = true, desc = 'LSP jump to prev error' })
+        "<cmd>lua require('lspsaga.diagnostic').goto_prev({ severity = vim.diagnostic.severity.ERROR })<CR>",
+        { silent = true, desc = 'LSP jump to prev error' })
 
     map('n', ']e',
-    "<cmd>lua require('lspsaga.diagnostic').goto_next({ severity = vim.diagnostic.severity.ERROR })<CR>",
-    { silent = true, desc = 'LSP jump to next error' })
+        "<cmd>lua require('lspsaga.diagnostic').goto_next({ severity = vim.diagnostic.severity.ERROR })<CR>",
+        { silent = true, desc = 'LSP jump to next error' })
 
     map('i', '<C-k>', '<cmd>Lspsaga hover_doc<CR>',
-    { silent = true, desc = 'LSP signature help' })
+        { silent = true, desc = 'LSP signature help' })
 
     map('n', '<C-k>', '<cmd>Lspsaga hover_doc<CR>',
-    { silent = true, desc = 'LSP signature help' })
+        { silent = true, desc = 'LSP signature help' })
 
     map('n', '<leader>ep', '<cmd>Lspsaga show_line_diagnostics<CR>',
-    { silent = true, desc = 'LSP line diagnostics' })
+        { silent = true, desc = 'LSP line diagnostics' })
 
     map('n', '<leader>eP', '<cmd>Lspsaga show_cursor_diagnostics<CR>',
-    { silent = true, desc = 'LSP cursor diagnostics' })
+        { silent = true, desc = 'LSP cursor diagnostics' })
 
     map('n', '<leader>lca', '<cmd>Lspsaga code_action<CR>',
-    { silent = true, desc = 'LSP code action' })
+        { silent = true, desc = 'LSP code action' })
 
     map('n', '<leader>ld', '<cmd>Lspsaga lsp_finder<CR>',
-    { silent = true, desc = 'LSP implementations' })
+        { silent = true, desc = 'LSP implementations' })
 
     map('n', '<leader>lco', '<cmd>Lspsaga outline<CR>',
-    { silent = true, desc = 'LSP code outline' })
+        { silent = true, desc = 'LSP code outline' })
 
     map('n', '<leader>lpf', '<cmd>Lspsaga peek_definition<CR>',
-    { silent = true, desc = 'LSP peek function definition' })
+        { silent = true, desc = 'LSP peek function definition' })
 
     map('n', '<leader>lr', '<cmd>Lspsaga rename<CR>',
-    { silent = true, desc = 'LSP rename' })
+        { silent = true, desc = 'LSP rename' })
 end
 
 return M
