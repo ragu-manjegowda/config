e254b25a9803f389b13f4bd3ba9a4a0501dcaa45
Ragu Manjegowda <raghavendrahm0410@gmail.com>
2023-01-23 18:16:15 -0800

    [update]: nvim, treesitter configs
    Details:
        Refactor dependency relation for treesitter module and assign
    correct lazy loading sequence.
    
    Resolves:
     * Improve startup time.
    
    Tests:
     * Tested on Mac Monterey with nvim 0.8.2
    
    Signed-off-by: Ragu Manjegowda <raghavendrahm0410@gmail.com>

--- .config/nvim/lua/user/treesitter.lua
+++ .config/nvim/lua/user/treesitter.lua
@@ -35,6 +35,53 @@ M.config = function()
             "vim",
             "yaml"
         },
+        textobjects = {
+            select = {
+                enable = true,
+
+                -- Automatically jump forward to textobj, similar to targets.vim
+                lookahead = true,
+
+                keymaps = {
+                    -- You can use the capture groups defined in textobjects.scm
+                    ["af"] = "@function.outer",
+                    ["if"] = "@function.inner",
+                    ["ac"] = "@class.outer",
+                    ["ic"] = "@class.inner",
+                },
+            },
+
+            move = {
+                enable = true,
+                set_jumps = true, -- whether to set jumps in the jumplist
+                goto_next_start = {
+                    ["]m"] = "@function.outer",
+                    ["]c"] = "@class.outer",
+                },
+                goto_next_end = {
+                    ["]M"] = "@function.outer",
+                    ["]C"] = "@class.outer",
+                },
+                goto_previous_start = {
+                    ["[m"] = "@function.outer",
+                    ["[c"] = "@class.outer",
+                },
+                goto_previous_end = {
+                    ["[M"] = "@function.outer",
+                    ["[C"] = "@class.outer",
+                },
+            },
+
+            lsp_interop = {
+                enable = true,
+                border = 'none',
+                peek_definition_code = {
+                    -- Offload this to lspsaga
+                    -- ["<leader>lpf"] = "@function.outer",
+                    -- ["<leader>lpc"] = "@class.outer",
+                },
+            },
+        }
     }
 end
 
