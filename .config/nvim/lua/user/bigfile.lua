c28232126733df07fd73f81d7245332ae733d23e
Ragu Manjegowda <raghavendrahm0410@gmail.com>
2023-02-17 12:49:33 -0800

    [cleanup]: nvim, remove bigfile plugin
    Details:
        Removed bigfile plugin as it does not seem to be useful, default
    plugins loaded by (n)/vim still makes it terribly slow.
    
    Resolves:
     * Slower response with large files, better off vim -u.
    
    Tests:
     * Tested on Mac with nvim 0.8.3
    
    Signed-off-by: Ragu Manjegowda <raghavendrahm0410@gmail.com>

--- .config/nvim/lua/user/bigfile.lua
+++ .config/nvim/lua/user/bigfile.lua
@@ -14,16 +14,40 @@ function M.setup()
         return
     end
 
+    local mini_indentscope = {
+        name = "mini_indentscope",
+        disable = function(buf)
+            vim.api.nvim_buf_set_var(buf, "miniindentscope_disable", 1)
+            require("gitsigns").detach(buf)
+        end,
+    }
+
     bigfile.config({
+        filesize = 1,
         features = {
+            "filetype",
             "indent_blankline",
             "lsp",
-            "treesitter",
-            "syntax",
             "matchparen",
-            "vimopts",
-            "filetype",
+            mini_indentscope,
+            "syntax",
+            "treesitter",
+            "vimopts"
         }
+    })
+
+    -- taken from AstroNvim
+    local big_file_events = { "BufRead", "BufWinEnter", "BufNewFile" }
+    vim.api.nvim_create_autocmd(big_file_events, {
+        once = true,
+        callback = function(args)
+            local buftype = vim.api.nvim_get_option_value("buftype",
+                { buf = args.buf })
+            if not (vim.fn.expand "%" == "" or buftype == "nofile") then
+                vim.cmd "do User FileOpened"
+                require("user.lspconfig").config()
+            end
+        end
     })
 end
 
