5320bda32eec10488c1462b9c37d7b2c78f2940c
Ragu Manjegowda <raghavendrahm0410@gmail.com>
2023-02-25 23:43:50 -0800

    [chor]: nvim, add surfingkeys config
    Details:
        Adding config for surfing keys extension.
    
    Resolves:
     * Surfing keys config portability.
    
    Tests:
     * Tested on Firefox with latest surfingkeys extension.
    
    Signed-off-by: Ragu Manjegowda <raghavendrahm0410@gmail.com>

---
+++ .config/misc/surfingkeys/config.js
@@ -0,0 +1,24 @@
+api.map('u', 'e');
+api.mapkey('p', "Open the clipboard's URL in the current tab", function() {
+    Front.getContentFromClipboard(function(response) {
+        window.location.href = response.data;
+    });
+});
+api.map('P', 'cc');
+api.map('H', 'S');
+api.map('L', 'D');
+api.map('gt', 'R');
+api.map('gT', 'E');
+api.unmap('gm');
+api.removeSearchAlias('b');
+api.removeSearchAlias('e');
+api.removeSearchAlias('h');
+api.removeSearchAlias('w');
+api.addSearchAlias('m', 'maps', 'http://maps.google.com/?q=', 's', 'https://www.google.com/complete/search?client=chrome-omni&gs_ri=chrome-ext&oit=1&cp=1&pgcl=7&q=', function(response) {
+        var res = JSON.parse(response.text);
+        return res[1];
+    });
+api.addSearchAlias('w', 'wikipedia', 'https://en.wikipedia.org/wiki/', 's', 'https://en.wikipedia.org/w/api.php?action=opensearch&format=json&formatversion=2&namespace=0&limit=40&search=', function(response) {
+        return JSON.parse(response.text)[1];
+    });
+api.addSearchAlias('h', 'github', 'https://github.com/search?q=', 's');
