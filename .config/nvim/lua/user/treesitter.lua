cfd7caa0bec1a24f45d9fa84688ae9d38bb05606
Ragu Manjegowda <raghavendrahm0410@gmail.com>
2023-04-17 03:50:38 -0700

    [feat]: nvim, enable semantic highlighting
    Details:
        Enable tree-sitter's semantic highlighting.
    
    Resolves:
     * None
    
    Tests:
     * Tested on Arch with nvim 0.9.0
    
    Signed-off-by: Ragu Manjegowda <raghavendrahm0410@gmail.com>

--- .config/nvim/lua/user/treesitter.lua
+++ .config/nvim/lua/user/treesitter.lua
@@ -73,18 +73,41 @@ M.config = function()
             },
 
             lsp_interop = {
-                enable = true,
+                enable = false,
                 border = 'none',
                 peek_definition_code = {
                     -- Offload this to lspsaga
                     -- ["<leader>lpf"] = "@function.outer",
                     -- ["<leader>lpc"] = "@class.outer",
                 },
-            },
+            }
         }
     }
 
     vim.opt["foldexpr"] = "nvim_treesitter#foldexpr()"
+
+    -- Setup new symantic highlighting
+    -- Reference: reddit.com/r/neovim/comments/12gvms4
+    local links = {
+        ['@lsp.type.namespace'] = '@namespace',
+        ['@lsp.type.type'] = '@type',
+        ['@lsp.type.class'] = '@type',
+        ['@lsp.type.enum'] = '@type',
+        ['@lsp.type.interface'] = '@type',
+        ['@lsp.type.struct'] = '@structure',
+        ['@lsp.type.parameter'] = '@parameter',
+        ['@lsp.type.variable'] = '@variable',
+        ['@lsp.type.property'] = '@property',
+        ['@lsp.type.enumMember'] = '@constant',
+        ['@lsp.type.function'] = '@function',
+        ['@lsp.type.method'] = '@method',
+        ['@lsp.type.macro'] = '@macro',
+        ['@lsp.type.decorator'] = '@function',
+    }
+
+    for newgroup, oldgroup in pairs(links) do
+        vim.api.nvim_set_hl(0, newgroup, { link = oldgroup, default = true })
+    end
 end
 
 return M
