a2707a449dca1aa2f41aa80df1e671703c174f77
Ragu Manjegowda <raghavendrahm0410@gmail.com>
2023-04-20 07:06:21 -0700

    [chor]: neomutt, refactor config
    Details:
        Refactored configs to put relevant files in their own folder.
    
    Resolves:
     * Configs in their own folder, housekeeping :p
    
    Tests:
     * Tested on Arch with neomutt 20230407
    
    Signed-off-by: Ragu Manjegowda <raghavendrahm0410@gmail.com>

--- .config/neomutt/create-alias.sh
+++ .config/neomutt/create-alias.sh
@@ -12,10 +12,10 @@ MESSAGE=$(cat)
 NEWALIAS=$(echo "${MESSAGE}" | grep ^"From: " | sed s/[\,\"\']//g | awk '{$1=""; if (NF == 3) {print "alias" $0;} else if (NF == 2) {print "alias" $0 $0;} else if (NF > 3) {print "alias", tolower($(NF-1))"-"tolower($2) $0;}}')
 
 # Remove links index (like [6]) added by w3m in mailcap
-NEWALIAS="${NEWALIAS//\[\([0-9]\+\)\]//}"
+NEWALIAS="$(echo "$NEWALIAS" | sed 's/\[\([0-9]\+\)\]//g')"
 
 # We never want to create some aliases
-NOALIAS_PAT="facebook|twitter|amazon|paypal|ops@exoscale.ch|noreply"
+NOALIAS_PAT="facebook|twitter|amazon|paypal|ops@exoscale.ch|noreply|no_reply"
 
 if grep -Eq "$NOALIAS_PAT" <<< "$NEWALIAS"; then
     :
