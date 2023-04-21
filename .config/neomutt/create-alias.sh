72a1f4ebaa91a9b5e9f222736760194566c30c51
Ragu Manjegowda <raghavendrahm0410@gmail.com>
2023-04-21 07:32:54 -0700

    [add]: neomutt, gmail support
    Details:
        Adding support for gmail.
    
    Resolves:
     * Gmail in neomutt.
    
    Tests:
     * Verified on Arch with neomutt 20230407
    
    Signed-off-by: Ragu Manjegowda <raghavendrahm0410@gmail.com>

--- .config/neomutt/create-alias.sh
+++ .config/neomutt/create-alias.sh
@@ -11,9 +11,11 @@ MESSAGE=$(cat)
 
 NEWALIAS=$(echo "${MESSAGE}" | grep ^"From: " | sed s/[\,\"\']//g | awk '{$1=""; if (NF == 3) {print "alias" $0;} else if (NF == 2) {print "alias" $0 $0;} else if (NF > 3) {print "alias", tolower($(NF-1))"-"tolower($2) $0;}}')
 
+# Remove links index (like [6]) added by w3m in mailcap
+NEWALIAS="${NEWALIAS//\[\([0-9]\+\)\]//}"
+
 # We never want to create some aliases
 NOALIAS_PAT="facebook|twitter|amazon|paypal|ops@exoscale.ch|noreply"
-
 
 if grep -Eq "$NOALIAS_PAT" <<< "$NEWALIAS"; then
     :
