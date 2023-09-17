bfbd1d3597ce67322ed92402c0b44a282167a772
Ragu Manjegowda <raghavendrahm0410@gmail.com>
2023-09-17 14:45:31 -0700

    [add]: nvim/brew, config
    Details:
        Added config to open link under cursor in neovim and added support
    to homebrew on M1/M2 Mac.
    
    Resolves:
     * Open link under cursor in neovim.
    
    Tests:
     * Tested on Arch linux with latest neovim.
    
    Signed-off-by: Ragu Manjegowda <raghavendrahm0410@gmail.com>

--- .config/nvim/lua/user/keymaps.lua
+++ .config/nvim/lua/user/keymaps.lua
@@ -138,3 +138,14 @@ keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
 
 -- Terminal exit insert mode
 keymap("t", "<leader><C-[>", "<C-\\><C-n>", opts)
+
+
+-- URL handling
+-- source: https://sbulav.github.io/vim/neovim-opening-urls/
+if vim.fn.has("mac") == 1 then
+    keymap("n", "gx", "<Cmd>call jobstart(['open', expand('<cfile>')], {'detach': v:true})<CR>", opts)
+elseif vim.fn.has("unix") == 1 then
+    keymap("n", "gx", "<Cmd>call jobstart(['xdg-open', expand('<cfile>')], {'detach': v:true})<CR>", opts)
+else
+    keymap("n", "gx", "<Cmd>lua print('Error: gx is not supported on this OS!')<CR>", opts)
+end
