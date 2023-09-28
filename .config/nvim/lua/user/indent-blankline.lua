d57ba66c3e3d76c09be56379c69072b62b1d0ef6
Ragu Manjegowda <raghavendrahm0410@gmail.com>
2023-09-28 13:39:30 -0700

    [fix]: mac/nvim, dircolors/indent blankline plugin
    Details:
     * Added OS check before running dircolor during toggle alacritty theme.
     * Fixed indent blankline plugin config after update.
    
    Resolves:
     * Dircolors not found error on Mac.
     * Autocmd error in nvim.
    
    Tests:
     * Tested on Mac with Nvim 0.9.2
    
    Signed-off-by: Ragu Manjegowda <raghavendrahm0410@gmail.com>

--- .config/nvim/lua/user/indent-blankline.lua
+++ .config/nvim/lua/user/indent-blankline.lua
@@ -7,29 +7,28 @@ local vim = vim
 
 local M = {}
 
-function M.before()
-    vim.g.indent_blankline_filetype_exclude = {
-            "lspinfo", "lazy", "checkhealth", "help", "man", "fugitive"
-        }
-end
-
 function M.config()
-    local res, indent_blankline = pcall(require, "indent_blankline")
+    local res, indent_blankline = pcall(require, "ibl")
     if not res then
-        vim.notify("indentscope not found", vim.log.levels.ERROR)
+        vim.notify("indent-blankline not found", vim.log.levels.ERROR)
         return
     end
 
     indent_blankline.setup({
         bufname_exclude = { "README.md" },
         char_list = { "|", "¦", "┆", "┊" },
+        exclude = {
+            filetypes = {
+                "lspinfo", "lazy", "checkhealth", "help", "man", "fugitive"
+            }
+        },
+        indent = { tab_char = "|" },
         space_char_blankline = " ",
         show_trailing_blankline_indent = false,
         show_current_context = true,
         use_treesitter = true,
         treesitter_scope = true
     })
-
 end
 
 return M
