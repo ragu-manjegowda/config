e2574a552b6dc6fbe8c8035ddb830baf7b4e43fe
Ragu Manjegowda <raghavendrahm0410@gmail.com>
2023-10-20 11:04:07 -0700

    [add]: nvim, animate/files plugin
    Details:
        Adding files and animate plugin.
    
    Resolves:
     * File editing withing explorer.
     * Cursor animation upon jump.
    
    Tests:
     * Tested on Arch with nvim 0.9.4
    
    Signed-off-by: Ragu Manjegowda <raghavendrahm0410@gmail.com>

---
+++ .config/nvim/lua/user/files.lua
@@ -0,0 +1,31 @@
+-------------------------------------------------------------------------------
+-- Author       : Ragu Manjegowda
+-- Github       : @ragu-manjegowda
+-------------------------------------------------------------------------------
+
+local vim = vim
+
+local M = {}
+
+function M.config()
+    local res, files = pcall(require, "mini.files")
+    if not res then
+        vim.notify("mini.files not found", vim.log.levels.ERROR)
+        return
+    end
+
+    files.setup()
+
+    vim.api.nvim_create_user_command(
+        'ToggleMiniFiles',
+        function()
+            if vim.g.minifiles_disable then
+                vim.g.minifiles_disable = false
+            else
+                vim.g.minifiles_disable = true
+            end
+        end, {}
+    )
+end
+
+return M
