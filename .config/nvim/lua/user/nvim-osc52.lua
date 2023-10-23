f9ae62fc9002af16676885cb34c729308cebda6c
Ragu Manjegowda <raghavendrahm0410@gmail.com>
2023-10-23 13:41:22 -0700

    [fix]: tmux, osc52 in nested tmux
    Details:
        Fix broken osc52 support in nested tmux mode.
    
    Resolves:
     * osc52 in nested tmux.
    
    Tests:
     * Tested on Arch and Mac Sonoma.
    
    Signed-off-by: Ragu Manjegowda <raghavendrahm0410@gmail.com>

--- .config/nvim/lua/user/nvim-osc52.lua
+++ .config/nvim/lua/user/nvim-osc52.lua
@@ -24,6 +24,13 @@ function M.config()
         return
     end
 
+    nvim_osc52.setup {
+        max_length = 0,     -- Maximum length of selection (0 for no limit)
+        silent = true,     -- Disable message on successful copy
+        trim = false,       -- Trim surrounding whitespaces before copy
+        tmux_passthrough = true, -- Use tmux passthrough (requires tmux: set -g allow-passthrough on)
+    }
+
     local function copy(lines, _)
         nvim_osc52.copy(table.concat(lines, '\n'))
     end
@@ -34,8 +41,8 @@ function M.config()
 
     vim.g.clipboard = {
         name = 'osc52',
-        copy = { ['+'] = copy,['*'] = copy },
-        paste = { ['+'] = paste,['*'] = paste },
+        copy = { ['+'] = copy, ['*'] = copy },
+        paste = { ['+'] = paste, ['*'] = paste },
     }
 
     -- Now the '+' register will copy to system clipboard using OSC52
@@ -44,4 +51,3 @@ function M.config()
 end
 
 return M
-
