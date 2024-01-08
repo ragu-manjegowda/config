f4f36539ae7d0b7b3a6df397608f3ec21ab108ab
Ragu Manjegowda <raghavendrahm0410@gmail.com>
2024-01-07 19:11:08 -0800

    [feat]: nvim, add flag to speed up
    Details:
        Added command line flags to disable few plugins which can speed up
    neovim.
        Remove tagbar plugin which was causing performance issues in CPP files.
        Added script to update branch for submodules.
    
    Resolves:
     * Nvim speed issues.
    
    Tests:
     * Tested on Arch with nvim 0.9.5
    
    Signed-off-by: Ragu Manjegowda <raghavendrahm0410@gmail.com>

--- .config/nvim/lua/user/plugins-table.lua
+++ .config/nvim/lua/user/plugins-table.lua
@@ -9,18 +9,6 @@ local core_plugins = {
         tag = "stable"
     },
 
-    -- Bazel
-    {
-        "alexander-born/bazel.nvim",
-        init = function()
-            require("user.bazel").before()
-        end,
-        dependencies = {
-            "nvim-treesitter/nvim-treesitter"
-        },
-        event = "VeryLazy"
-    },
-
     -- Clipboard
     {
         "ojroques/nvim-osc52",
@@ -51,24 +39,6 @@ local core_plugins = {
         }
     },
 
-    -- Code Browsing
-    -- {
-    --     "skywind3000/gutentags_plus",
-    --     dependencies = { "ludovicchabant/vim-gutentags" },
-    --     config = function()
-    --         require("user.gutentags-plus").config()
-    --     end
-    -- },
-
-    { "derekwyatt/vim-fswitch" },
-
-    {
-        "preservim/tagbar",
-        init = function()
-            require("user.tagbar").before()
-        end
-    },
-
     -- Codeium
     {
         "Exafunction/codeium.vim",
@@ -78,18 +48,6 @@ local core_plugins = {
         event = { "InsertEnter" },
     },
 
-    -- Compile
-    {
-        "tpope/vim-dispatch"
-    },
-
-    -- Cursor animation
-    {
-        "gen740/SmoothCursor.nvim",
-        config = function()
-            require("user.smoothcursor").config()
-        end
-    },
 
     -- Debugging
     {
@@ -112,16 +70,6 @@ local core_plugins = {
         event = "VeryLazy"
     },
 
-    -- Extras
-    {
-        "echasnovski/mini.bracketed",
-        event = "VeryLazy",
-        config = function()
-            require("user.bracketed").config()
-        end,
-        version = false
-    },
-
     {
         "nvim-tree/nvim-tree.lua",
         dependencies = { "nvim-tree/nvim-web-devicons" },
@@ -129,17 +77,6 @@ local core_plugins = {
             require("user.nvim-tree").config()
         end
     },
-
-    {
-        "echasnovski/mini.files",
-        config = function()
-            require("user.files").config()
-        end,
-        event = "VeryLazy",
-        version = false
-    },
-
-    { "itchyny/vim-cursorword" },
 
     {
         "folke/which-key.nvim",
@@ -150,19 +87,6 @@ local core_plugins = {
             require("user.which-key").config()
         end,
         event = "VeryLazy"
-    },
-
-    -- Indent markers
-    {
-        "lukas-reineke/indent-blankline.nvim",
-        config = function()
-            require("user.indent-blankline").config()
-        end,
-        dependencies = {
-            "nvim-treesitter/nvim-treesitter",
-        },
-        event = "UIEnter",
-        main = "ibl"
     },
 
     -- Terminal
@@ -182,14 +106,6 @@ local core_plugins = {
         priority = 1001,
     },
 
-
-    {
-        "Rasukarusan/nvim-select-multi-line",
-        init = function()
-            require("user.nvim-select-multi-line").before()
-        end
-    },
-
     { "lambdalisue/suda.vim" },
 
     -- Git
@@ -197,84 +113,6 @@ local core_plugins = {
         "tpope/vim-fugitive",
         config = function()
             require("user.fugitive").config()
-        end
-    },
-
-    {
-        "aaronhallaert/advanced-git-search.nvim",
-        config = function()
-            require("telescope").load_extension("advanced_git_search")
-        end,
-        dependencies = {
-            "nvim-telescope/telescope.nvim",
-            -- to show diff splits and open commits in browser
-            "tpope/vim-fugitive"
-        },
-        event = "UIEnter"
-    },
-
-    {
-        "fredeeb/tardis.nvim",
-        config = true,
-        dependencies = { "nvim-lua/plenary.nvim" },
-        event = "VeryLazy"
-    },
-
-    {
-        "lewis6991/gitsigns.nvim",
-        config = function()
-            require("user.gitsigns").config()
-        end
-    },
-
-    -- Leetcode in Vim
-    {
-        "Dhanus3133/LeetBuddy.nvim",
-        config = function()
-            require("user.leetbuddy").config()
-        end,
-        dependencies = {
-            "nvim-lua/plenary.nvim",
-            "nvim-telescope/telescope.nvim",
-        },
-        event = "VeryLazy"
-    },
-
-    -- LSP
-    {
-        "neovim/nvim-lspconfig",
-        dependencies = {
-            {
-                "lvimuser/lsp-inlayhints.nvim",
-                config = function()
-                    require("lsp-inlayhints").setup()
-                end
-            },
-            "ray-x/lsp_signature.nvim"
-        },
-        config = function()
-            require("user.lspconfig").config()
-        end,
-        event = "BufReadPre"
-    },
-
-    -- LSP Saga
-    {
-        "glepnir/lspsaga.nvim",
-        branch = "main",
-        dependencies = {
-            "neovim/nvim-lspconfig"
-        },
-        config = function()
-            require("user.lspsaga").config()
-        end,
-        event = "BufRead"
-    },
-
-    {
-        "Mofiqul/trld.nvim",
-        config = function()
-            require("trld").setup()
         end
     },
 
@@ -334,20 +172,6 @@ local core_plugins = {
         event = "UIEnter"
     },
 
-    -- Treesitter
-    {
-        "nvim-treesitter/nvim-treesitter",
-        dependencies = {
-            "nvim-treesitter/nvim-treesitter-textobjects",
-            lazy = true,
-        },
-        build = ":TSUpdate",
-        config = function()
-            require("user.treesitter").config()
-        end,
-        event = "BufRead"
-    },
-
     -- Vim Session
     {
         "xolox/vim-session",
@@ -362,4 +186,211 @@ local core_plugins = {
     }
 }
 
-return core_plugins
+local extra_plugins = {
+    core_plugins,
+
+    -- LSP
+    {
+        "neovim/nvim-lspconfig",
+        dependencies = {
+            {
+                "lvimuser/lsp-inlayhints.nvim",
+                config = function()
+                    require("lsp-inlayhints").setup()
+                end
+            },
+            "ray-x/lsp_signature.nvim"
+        },
+        config = function()
+            require("user.lspconfig").config()
+        end,
+        event = "BufReadPre"
+    },
+
+    -- LSP Saga
+    {
+        "glepnir/lspsaga.nvim",
+        branch = "main",
+        dependencies = {
+            "neovim/nvim-lspconfig"
+        },
+        config = function()
+            require("user.lspsaga").config()
+        end,
+        event = "BufRead"
+    },
+
+    {
+        "Mofiqul/trld.nvim",
+        config = function()
+            require("trld").setup()
+        end
+    }
+}
+
+local all_plugins = {
+    extra_plugins,
+
+    -- Bazel
+    {
+        "alexander-born/bazel.nvim",
+        init = function()
+            require("user.bazel").before()
+        end,
+        dependencies = {
+            "nvim-treesitter/nvim-treesitter"
+        },
+        event = "VeryLazy"
+    },
+
+    -- Code Browsing
+    -- {
+    --     "skywind3000/gutentags_plus",
+    --     dependencies = { "ludovicchabant/vim-gutentags" },
+    --     config = function()
+    --         require("user.gutentags-plus").config()
+    --     end
+    -- },
+
+    { "derekwyatt/vim-fswitch" },
+
+    -- Compile
+    {
+        "tpope/vim-dispatch"
+    },
+
+    -- Cursor animation
+    {
+        "gen740/SmoothCursor.nvim",
+        config = function()
+            require("user.smoothcursor").config()
+        end
+    },
+
+    -- Extras
+    {
+        "echasnovski/mini.bracketed",
+        event = "VeryLazy",
+        config = function()
+            require("user.bracketed").config()
+        end,
+        version = false
+    },
+
+    {
+        "echasnovski/mini.files",
+        config = function()
+            require("user.files").config()
+        end,
+        event = "VeryLazy",
+        version = false
+    },
+
+    { "itchyny/vim-cursorword" },
+
+    -- Indent markers
+    {
+        "lukas-reineke/indent-blankline.nvim",
+        config = function()
+            require("user.indent-blankline").config()
+        end,
+        dependencies = {
+            "nvim-treesitter/nvim-treesitter",
+        },
+        event = "UIEnter",
+        main = "ibl"
+    },
+
+    {
+        "Rasukarusan/nvim-select-multi-line",
+        init = function()
+            require("user.nvim-select-multi-line").before()
+        end
+    },
+
+    {
+        "lewis6991/gitsigns.nvim",
+        config = function()
+            require("user.gitsigns").config()
+        end
+    },
+
+    {
+        "aaronhallaert/advanced-git-search.nvim",
+        config = function()
+            require("telescope").load_extension("advanced_git_search")
+        end,
+        dependencies = {
+            "nvim-telescope/telescope.nvim",
+            -- to show diff splits and open commits in browser
+            "tpope/vim-fugitive"
+        },
+        event = "UIEnter"
+    },
+
+    {
+        "fredeeb/tardis.nvim",
+        config = true,
+        dependencies = { "nvim-lua/plenary.nvim" },
+        event = "VeryLazy"
+    },
+
+    -- Leetcode in Vim
+    {
+        "Dhanus3133/LeetBuddy.nvim",
+        config = function()
+            require("user.leetbuddy").config()
+        end,
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-telescope/telescope.nvim",
+        },
+        event = "VeryLazy"
+    },
+
+    -- Treesitter
+    {
+        "nvim-treesitter/nvim-treesitter",
+        dependencies = {
+            "nvim-treesitter/nvim-treesitter-textobjects",
+            lazy = true,
+        },
+        build = ":TSUpdate",
+        config = function()
+            require("user.treesitter").config()
+        end,
+        event = "BufRead"
+    }
+}
+
+-------------------------------------------------------------------------------
+-- Check if we need to load all plugins
+-- Workaround for disabling plugins known to slow down neovim on certain files
+--
+--   vim --cmd "+lua vim.g.blazing_fast = 0" -- load all plugins
+--   vim --cmd "+lua vim.g.blazing_fast = 1" -- load core + extra plugins
+--   vim --cmd "+lua vim.g.blazing_fast = 2" -- load core plugins only
+--   vim -u NORC                             -- do not load user config
+--   vim --noplugin                          -- do not load plugins
+--
+-------------------------------------------------------------------------------
+if vim.g.blazing_fast == 1 then
+    return extra_plugins
+elseif vim.g.blazing_fast == 2 then
+    -- Set options to make nvim faster
+    local options = {
+        foldmethod = "manual",
+        list = false,
+        swapfile = false,
+        syntax = "disable",
+        undolevels = -1,
+        undoreload = 0
+    }
+    for k, v in pairs(options) do
+        vim.opt[k] = v
+    end
+
+    return core_plugins
+else
+    return all_plugins
+end
---
+++ .config/scripts/update_submodules.sh
@@ -0,0 +1,48 @@
+pushd .
+
+cd ~/.config/awesome/library/battery
+git checkout master
+
+cd ~/.config/awesome/library/bling
+git checkout master
+
+cd ~/.config/awesome/library/revelation
+git checkout master
+
+cd ~/.config/rEFInd/refind-theme-regular
+git checkout master
+
+cd ~/.config/ranger/plugins/ranger_fzf_filter
+git checkout main
+
+cd ~/.config/tmux/plugins/tmux-continuum
+git checkout master
+
+cd ~/.config/tmux/plugins/tmux-fuzzback
+git checkout main
+
+cd ~/.config/tmux/plugins/tmux-resurrect
+git checkout master
+
+cd ~/.config/zsh/oh-my-zsh
+git checkout master
+
+cd ~/.config/zsh/zsh-custom/plugins/fzf-tab
+git checkout master
+
+cd ~/.config/zsh/zsh-custom/plugins/fzf-tab-source
+git checkout main
+
+cd ~/.config/zsh/zsh-custom/plugins/zsh-autosuggestions
+git checkout master
+
+cd ~/.config/zsh/zsh-custom/plugins/zsh-hist
+git checkout main
+
+cd ~/.config/zsh/zsh-custom/themes/dircolors-solarized
+git checkout master
+
+cd ~/.config/zsh/zsh-custom/themes/powerlevel10k
+git checkout master
+
+popd
