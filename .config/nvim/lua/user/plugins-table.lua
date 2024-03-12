4bf81fb4707acb3f3b20fabad482c08b68f40cd2
Ragu Manjegowda <raghavendrahm0410@gmail.com>
2024-03-12 13:55:34 -0700

    [add]: nvim, git diffview
    Details:
        Add git diffview plugin, removed advanced-git-search since it seems
    to be broken.
    
    Resolves:
     * Git diff view of commits.
    
    Tests:
     * Tested on Arch with git 2.44.0
    
    Signed-off-by: Ragu Manjegowda <raghavendrahm0410@gmail.com>

--- .config/nvim/lua/user/plugins-table.lua
+++ .config/nvim/lua/user/plugins-table.lua
@@ -316,16 +316,8 @@ local all_plugins = {
     },
 
     {
-        "aaronhallaert/advanced-git-search.nvim",
-        config = function()
-            require("telescope").load_extension("advanced_git_search")
-        end,
-        dependencies = {
-            "nvim-telescope/telescope.nvim",
-            -- to show diff splits and open commits in browser
-            "tpope/vim-fugitive"
-        },
-        event = "UIEnter"
+        "sindrets/diffview.nvim",
+        event = "VeryLazy"
     },
 
     {
