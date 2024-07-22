ecf4ccf41d487dbf51278d1f19e124f7a1d58d00
Ragu Manjegowda <raghavendrahm0410@gmail.com>
2024-07-21 19:31:13 -0700

    [update]: nvim, update which-key configs
    Details:
        Fix warnings from which-key plugin.
    
    Resolves:
     * Which-key warnings.
    
    Tests:
     * Tested on Arch with nvim 0.10.0
    
    Signed-off-by: Ragu Manjegowda <raghavendrahm0410@gmail.com>

--- .config/nvim/lua/user/which-key.lua
+++ .config/nvim/lua/user/which-key.lua
@@ -44,9 +44,6 @@ function M.config()
                 suggestions = 20
             },
             presets = {
-                -- adds help for operators like d, y, ... and registers them
-                -- for motion / text object completion
-                operators = false,
                 motions = false, -- adds help for motions
                 -- help for text objects triggered after entering an operator
                 text_objects = false,
@@ -56,10 +53,7 @@ function M.config()
                 g = true -- bindings for prefixed with g
             }
         },
-        -- add operators that will trigger motion and text object completion
-        -- to enable all native operators, set the preset / operators plugin above
-        operators = { gc = "Comments" },
-        key_labels = {
+        replace = {
             -- override the label used to display some keys.
             -- It doesn't effect WK in any other way.
             -- For example:
@@ -73,27 +67,13 @@ function M.config()
             separator = "➜", -- symbol used between a key and it's label
             group = "+" -- symbol prepended to a group
         },
-        window = {
-            border = "none", -- none, single, double, shadow
-            position = "bottom", -- bottom, top
-            margin = { 1, 0, 1, 0 }, -- extra window margin [top, right, bottom, left]
-            padding = { 1, 1, 1, 1 } -- extra window padding [top, right, bottom, left]
-        },
         layout = {
             height = { min = 4, max = 25 }, -- min and max height of the columns
             width = { min = 20, max = 50 }, -- min and max width of the columns
             spacing = 5 -- spacing between columns
         },
-        -- hide mapping boilerplate
-        hidden = {
-            "<silent>", "<cmd>", "<Cmd>", "<CR>", "call",
-            "lua", "^:", "^ "
-        },
         -- show help message on the command line when the popup is visible
         show_help = true,
-        -- automatically setup triggers
-        triggers = "auto"
-        -- triggers = {"<leader>"} -- or specifiy a list manually
     }
 end
 
