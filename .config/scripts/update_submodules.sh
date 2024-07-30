c0e295a7c8cf3c3310fa9d331e1de31e7eb597b0
Ragu Manjegowda <raghavendrahm0410@gmail.com>
2024-07-30 16:21:29 -0700

    [fix]: alacritty, colors definition path
    Details:
        Avoid using absolute path for colors definition.
    
    Resolves:
     * Cross platform errors for mismatch in absolute path.
    
    Tests:
     * Tested on Arch and Mac with alacritty 0.13.2
    
    Signed-off-by: Ragu Manjegowda <raghavendrahm0410@gmail.com>

--- .config/scripts/update_submodules.sh
+++ .config/scripts/update_submodules.sh
@@ -1,54 +1,57 @@
 pushd .
 
-cd ~/.config/awesome/library/battery
+cd ~/.config/awesome/library/battery || exit
 git checkout master
 
-cd ~/.config/awesome/library/bling
+cd ~/.config/awesome/library/bling || exit
 git checkout master
 
-cd ~/.config/awesome/library/revelation
+cd ~/.config/awesome/library/revelation || exit
 git checkout master
 
-cd ~/.config/fzf-git
+cd ~/.config/fzf-git || exit
 git checkout main
 
-cd ~/.config/rEFInd/refind-theme-regular
-git checkout master
-
-cd ~/.config/ranger/plugins/ranger_fzf_filter
+cd ~/.config/mpv/scripts/autosubsync || exit
 git checkout main
 
-cd ~/.config/ranger/plugins/ranger_devicons
+cd ~/.config/rEFInd/refind-theme-regular || exit
+git checkout master
+
+cd ~/.config/ranger/plugins/ranger_fzf_filter || exit
 git checkout main
 
-cd ~/.config/tmux/plugins/tmux-continuum
-git checkout master
-
-cd ~/.config/tmux/plugins/tmux-fuzzback
+cd ~/.config/ranger/plugins/ranger_devicons || exit
 git checkout main
 
-cd ~/.config/tmux/plugins/tmux-resurrect
+cd ~/.config/tmux/plugins/tmux-continuum || exit
 git checkout master
 
-cd ~/.config/zsh/oh-my-zsh
-git checkout master
-
-cd ~/.config/zsh/zsh-custom/plugins/fzf-tab
-git checkout master
-
-cd ~/.config/zsh/zsh-custom/plugins/fzf-tab-source
+cd ~/.config/tmux/plugins/tmux-fuzzback || exit
 git checkout main
 
-cd ~/.config/zsh/zsh-custom/plugins/zsh-autosuggestions
+cd ~/.config/tmux/plugins/tmux-resurrect || exit
 git checkout master
 
-cd ~/.config/zsh/zsh-custom/plugins/zsh-hist
+cd ~/.config/zsh/oh-my-zsh || exit
+git checkout master
+
+cd ~/.config/zsh/zsh-custom/plugins/fzf-tab || exit
+git checkout master
+
+cd ~/.config/zsh/zsh-custom/plugins/fzf-tab-source || exit
 git checkout main
 
-cd ~/.config/zsh/zsh-custom/themes/dircolors-solarized
+cd ~/.config/zsh/zsh-custom/plugins/zsh-autosuggestions || exit
 git checkout master
 
-cd ~/.config/zsh/zsh-custom/themes/powerlevel10k
+cd ~/.config/zsh/zsh-custom/plugins/zsh-hist || exit
+git checkout main
+
+cd ~/.config/zsh/zsh-custom/themes/dircolors-solarized || exit
 git checkout master
 
-popd
+cd ~/.config/zsh/zsh-custom/themes/powerlevel10k || exit
+git checkout master
+
+popd || exit
