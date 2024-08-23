1485097ff2b1dc4f1ba4a170f683e95b39e5ca6d
Ragu Manjegowda <raghavendrahm0410@gmail.com>
2024-08-23 00:22:15 -0700

    [add]: nvim, remote-nvim plugin
    Details:
        Add remote-nvim plugin, making it easy to copy over and setup
    nvim configs to remote dev machines.
    
    Resolves:
     * nvim setup on temp dev machines.
    
    Tests:
     * Tested on Arch+Ubuntu22 with nvim v0.10.1
    
    Signed-off-by: Ragu Manjegowda <raghavendrahm0410@gmail.com>

--- .config/nvim/lua/user/noice.lua
+++ .config/nvim/lua/user/noice.lua
@@ -29,12 +29,25 @@ function M.config()
             hover = { enabled = false },
             signature = { enabled = false }
         },
+        notify = {
+            replace = true
+        },
         presets = {
             bottom_search = true,         -- use a classic bottom cmdline for search
             command_palette = true,       -- position the cmdline and popupmenu together
             long_message_to_split = true, -- long messages will be sent to a split
             inc_rename = false,           -- enables an input dialog for inc-rename.nvim
             lsp_doc_border = false,       -- add a border to hover docs and signature help
+        },
+        routes = {
+            {
+                filter = {
+                    event = "msg_show",
+                    kind = "",
+                    find = "written",
+                },
+                opts = { skip = true },
+            }
         }
     })
 
