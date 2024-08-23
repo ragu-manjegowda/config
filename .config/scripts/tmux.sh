c4f5cba9edd4175bca1ec4498ca71528a3967348
Ragu Manjegowda <raghavendrahm0410@gmail.com>
2024-08-23 00:24:09 -0700

    [add]: config, scripts for appimage
    Details:
        Add scripts for nvim and tmux appimage to get latest release and
    config compatibility with temp Ubuntu machines.
    
    Resolves:
     * Setting up tmux and nvim on dev machines.
    
    Tests:
     * Tested on Ubuntu 22.04 with nvim v0.10.1 and tmux 3.3a
    
    Signed-off-by: Ragu Manjegowda <raghavendrahm0410@gmail.com>

---
+++ .config/scripts/tmux.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+TMUX_BIN_PATH=/localhome/local-rmanjegowda/.remote-nvim/tmux-downloads/tmux.appimage
+TMUX_INSTALL_DIR=$(dirname "$TMUX_BIN_PATH")
+
+if [ ! -f "$TMUX_BIN_PATH" ]; then
+    mkdir -p "$TMUX_INSTALL_DIR"
+    cd "$TMUX_INSTALL_DIR" || { echo "Failed to change directory to $TMUX_INSTALL_DIR"; exit 1; }
+
+    curl -s https://api.github.com/repos/nelsonenzo/tmux-appimage/releases/latest \
+    | grep "browser_download_url.*appimage" \
+    | cut -d : -f 2,3 \
+    | tr -d \" \
+    | wget -qi - \
+    && chmod +x tmux.appimage
+fi
+
+### rsync command to copy tmux config
+# rsync -avz --no-perms --no-owner --no-group --no-links -e \
+# "ssh -F /home/ragu/.ssh/config" ~/.config/tmux/ \
+# nv_ovx_local_lovelace:/localhome/local-rmanjegowda/.config/tmux
+
+
+$TMUX_BIN_PATH -L "appimage" "$@"
