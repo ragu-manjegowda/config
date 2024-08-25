394765edf519e93cbdd0b9d0b777e2566484df4b
Ragu Manjegowda <raghavendrahm0410@gmail.com>
2024-08-24 17:41:54 -0700

    [update]: awesomewm, lenovo config
    Details:
        Adding updated awesomwm config compatible with 14inch laptop.
    
    Resolves:
     * Layout size compatible with 14 inch screen.
    
    Tests:
     * Tested on 14inch Lenovo Yoga X1.
    
    Signed-off-by: Ragu Manjegowda <raghavendrahm0410@gmail.com>

--- .config/awesome/configuration/config.lua
+++ .config/awesome/configuration/config.lua
@@ -30,9 +30,9 @@ return {
 
     keyboard = {
         script = utils_dir .. 'kbd-bkl',
-        file = '/sys/class/leds/dell::kbd_backlight/brightness'
+        -- file = '/sys/class/leds/dell::kbd_backlight/brightness'
         -- file = '/sys/class/leds/smc::kbd_backlight/brightness'
-        -- file = '/sys/class/leds/tpacpi::kbd_backlight/brightness'
+        file = '/sys/class/leds/tpacpi::kbd_backlight/brightness'
     },
 
     widget = {
