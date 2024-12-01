66612d60fb9f0a1570e6f9e7a20b9022b17ece97
Ragu Manjegowda <raghavendrahm0410@gmail.com>
2024-11-30 23:24:32 -0800

    [chore]: config, backup potatofox config
    Details:
        Backup changes made to potatofox config.
    
    Resolves:
     * Custom changes to potatofox configs.
    
    Tests:
     * N/A
    
    Signed-off-by: Ragu Manjegowda <raghavendrahm0410@gmail.com>

---
+++ .config/firefox/user.js
@@ -0,0 +1,58 @@
+// userchrome.css usercontent.css activate
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+
+// fill svg color
+user_pref("svg.context-properties.content.enabled", true);
+
+// enable :has selector
+user_pref("layout.css.has-selector.enabled", true);
+
+// integrated calculator at urlbar
+user_pref("browser.urlbar.suggest.calculator", true);
+
+// integrated unit convertor at urlbar
+user_pref("browser.urlbar.unitConversion.enabled", true);
+
+// trim url
+user_pref("browser.urlbar.trimHttps", true);
+user_pref("browser.urlbar.trimURLs", true);
+
+// show profile management in hamburger menu
+user_pref("browser.profiles.enabled", true);
+
+// gtk rounded corners
+user_pref("widget.gtk.rounded-bottom-corners.enabled", true);
+
+// show compact mode
+user_pref("browser.compactmode.show", true);
+
+// fix sidebar tab drag on linux
+user_pref("widget.gtk.ignore-bogus-leave-notify", 1);
+
+user_pref("browser.tabs.allow_transparent_browser", true);
+
+// uidensity -> compact
+user_pref("browser.uidensity", 1);
+
+// macos transparent
+user_pref("widget.macos.titlebar-blend-mode.behind-window", true);
+
+// don't warn on about:config open
+user_pref("browser.aboutConfig.showWarning", false);
+
+user_pref("sidebar.revamp", false);
+user_pref("sidebar.verticalTabs", false);
+
+user_pref("uc.tweak.borderless", true);
+user_pref("uc.tweak.borderless.no-round", true);
+user_pref("uc.tweak.no-custom-icons", true);
+user_pref("uc.tweak.no-window-controls", true);
+user_pref("uc.tweak.sidebar.wide", true);
+user_pref("uc.tweak.sidebar.header", false);
+user_pref("uc.tweak.translucency", true);
+
+user_pref("cookiebanners.service.mode", 2);
+user_pref("cookiebanners.service.mode.privateBrowsing", 2);
+user_pref("browser.quitShortcut.disabled", true);
+user_pref("browser.fullscreen.exit_on_escape", false);
+user_pref("widget.use-xdg-desktop-portal.file-picker", 1);
