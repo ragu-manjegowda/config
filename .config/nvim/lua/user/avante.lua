cae46c34cc033250eab154c749d1d1c7a1d8837c
Ragu Manjegowda <raghavendrahm0410@gmail.com>
2025-01-22 20:10:07 -0800

    [feat]: nvim, avante plugin
    Details:
        Adding avante plugin support.
    
    Resolves:
     * AI code chat.
    
    Tests:
     * Tested with nvim v0.10.13
    
    Signed-off-by: Ragu Manjegowda <raghavendrahm0410@gmail.com>

---
+++ .config/nvim/lua/user/avante.lua
@@ -0,0 +1,36 @@
+-------------------------------------------------------------------------------
+-- Author       : Ragu Manjegowda
+-- Github       : @ragu-manjegowda
+-------------------------------------------------------------------------------
+
+local M = {}
+
+function M.opts()
+    return {
+        -- add any opts here
+        provider = "openai",
+        openai = {
+            endpoint = "https://integrate.api.nvidia.com/v1",
+            model = "meta/llama-3.1-405b-instruct"
+            -- model = "nvdev/nvidia/llama-3.1-nemotron-70b-instruct"
+        },
+        azure = {}
+    }
+end
+
+function M.img_clip_opts()
+    return {
+        -- recommended settings
+        default = {
+            embed_image_as_base64 = false,
+            prompt_for_file_name = false,
+            drag_and_drop = {
+                insert_mode = true,
+            },
+            -- required for Windows users
+            use_absolute_path = true
+        }
+    }
+end
+
+return M
