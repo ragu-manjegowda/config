a3fd2758f45b94890ffa56fec1895eeafb6e2bf7
Ragu Manjegowda <raghavendrahm0410@gmail.com>
2025-09-28 00:10:53 -0700

    [fix]: perf, lessfilter stable process
    Details:
     * Fix performance issue due to stale process from lessfilter.
     * Update monitor setup options.
     * Disabled setting colemak-dh as default since external keyboard is
        programmed to use that layout.
     * Move lua-lgi to lua51-lgi
    
    Resolves:
     * Performance issue with lessfilter/fzf.
    
    Tests:
     * Tested locally on Arch.
    
    Signed-off-by: Ragu Manjegowda <raghavendrahm0410@gmail.com>

--- .config/nvim/lua/user/codecompanion.lua
+++ .config/nvim/lua/user/codecompanion.lua
@@ -8,20 +8,22 @@ local M = {}
 function M.opts()
     return {
         adapters = {
-            openai = function()
-                return require("codecompanion.adapters").extend("openai", {
-                    url = "https://integrate.api.nvidia.com/v1/chat/completions",
-                    env = {
-                        api_key = os.getenv("OPENAI_API_KEY"),
-                    },
-                    schema = {
-                        model = {
-                            -- default = "nvdev/nvidia/llama-3.1-nemotron-70b-instruct"
-                            default = "nvdev/qwen/qwen2.5-coder-32b-instruct"
+            http = {
+                openai = function()
+                    return require("codecompanion.adapters").extend("openai", {
+                        url = "https://integrate.api.nvidia.com/v1/chat/completions",
+                        env = {
+                            api_key = os.getenv("OPENAI_API_KEY"),
                         },
-                    },
-                })
-            end,
+                        schema = {
+                            model = {
+                                -- default = "nvdev/nvidia/llama-3.1-nemotron-70b-instruct"
+                                default = "nvdev/qwen/qwen2.5-coder-32b-instruct"
+                            }
+                        }
+                    })
+                end
+            }
         },
         strategies = {
             -- Change the default chat adapter
@@ -34,7 +36,7 @@ function M.opts()
                 callback = "mcphub.extensions.codecompanion",
                 opts = {
                     show_result_in_chat = true, -- Show mcp tool results in chat
-                    make_vars = true,     -- Convert resources to #variables
+                    make_vars = true,           -- Convert resources to #variables
                     make_slash_commands = true, -- Add prompts as /slash commands
                 }
             }
